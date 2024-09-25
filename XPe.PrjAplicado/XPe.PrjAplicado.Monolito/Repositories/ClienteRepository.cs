1a7f81f012d2ba21eb5f75474e5b716a28787770
Everaldo Araujo <everaldo.araujo@mxm.com.br>
2024-09-25 17:09:22 -0300

    Criando repositorios fake para alimentar a aplicação

--- XPe.PrjAplicado/XPe.PrjAplicado.Monolito/Repositories/ClienteRepository.cs
+++ XPe.PrjAplicado/XPe.PrjAplicado.Monolito/Repositories/ClienteRepository.cs
@@ -2,36 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using XPe.PrjAplicado.Monolito.API.Entities;
+using XPe.PrjAplicado.Monolito.API.Repositories.DbContext;
 
 namespace XPe.PrjAplicado.Monolito.API.Repositories
 {
     public class ClienteRepository
     {
-        private IList<Cliente> _dados;
-
-        public ClienteRepository()
+        public IList<Cliente> Clientes
         {
-            _dados = new List<Cliente>();
+            get 
+            { 
+                return FakePrjAplicadoDbContext.Clientes; 
+            }
         }
 
         public Cliente Obter(Guid codigo)
         {
-            return _dados.FirstOrDefault(C => C.Codigo == codigo);
+            return Clientes.FirstOrDefault(C => C.Codigo == codigo);
         }
 
         public IList<Cliente> ObterLista()
         {
-            return _dados.ToList();
+            return Clientes.ToList();
         }
 
         public void Salvar(Cliente cliente)
         {
-            _dados.Add(cliente);
+            Clientes.Add(cliente);
         }
 
         public void Atualizar(Guid codigo, Cliente cliente)
         {
-            var entidade = _dados.FirstOrDefault(C => C.Codigo == codigo);
+            var entidade = Clientes.FirstOrDefault(C => C.Codigo == codigo);
 
             if (entidade != null)
                 entidade = cliente;
@@ -44,7 +46,7 @@ namespace XPe.PrjAplicado.Monolito.API.Repositories
             var entidade = Obter(codigo);
 
             if (entidade != null)
-                _dados.Remove(entidade);
+                Clientes.Remove(entidade);
             else
                 throw new Exception("Dados não encontrados!");
         }
---
+++ XPe.PrjAplicado/XPe.PrjAplicado.Monolito/Repositories/DbContext/FakePrjAplicadoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using XPe.PrjAplicado.Monolito.API.Entities;
+
+namespace XPe.PrjAplicado.Monolito.API.Repositories.DbContext
+{
+    public static class FakePrjAplicadoDbContext
+    {
+        public static IList<Cliente> Clientes { get; set; } = new List<Cliente>();
+        public static IList<Produto> Produtos { get; set; } = new List<Produto>();
+        public static IList<Pedido> Pedidos { get; set; } = new List<Pedido>();
+    }
+}
